7fa01307a5e9c419dc760d40955fb91fcd1cb00d
gavransava <gavransava@gmail.com>
2017-11-27 12:28:18 +0100

    Added dependencies and project structure.

--- app/src/main/java/savagavran/blorgreader/main/MainActivity.java
+++ app/src/main/java/savagavran/blorgreader/main/MainActivity.java
@@ -1,7 +1,9 @@
-package savagavran.blorgreader;
+package savagavran.blorgreader.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import savagavran.blorgreader.R;
 
 public class MainActivity extends AppCompatActivity {
 
